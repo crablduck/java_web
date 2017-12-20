d064311378daffade92d7ce0f2b8f9d1695c786e
crablduck <asdfg756868947@qq.com>
2017-12-20 16:07:53 +0800

    servlet_local_login

---
+++ login_servlet/src/com/xjw/servlet/LoginServlet.java
@@ -0,0 +1,59 @@
+package com.xjw.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	
+    	System.out.println("进来了");
+    	
+    	request.setCharacterEncoding("utf-8");
+    	response.setContentType("text/html;charset=utf-8");
+    	
+    	String username = request.getParameter("username");
+    	String password = request.getParameter("password");
+    	
+    	PrintWriter writer = response.getWriter();
+    	if("admin".equals(username) && "123".equals(password)){
+    		writer.write("登录成功");
+    	}else{
+    		writer.write("登录失败");
+    	}
+    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
